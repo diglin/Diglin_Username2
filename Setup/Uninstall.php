18f3254ea0ed7b5983b7a44bb6b94b203c5225b4
Sylvain Rayé <sly@diglin.com>
2016-08-29 13:21:43 +0200

    Feature #15: Add uninstall script

---
+++ Setup/Uninstall.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Diglin GmbH - Switzerland
+ *
+ * @author      Sylvain Rayé <support at diglin.com>
+ * @category    Diglin
+ * @package     Diglin_
+ * @copyright   Copyright (c) 2011-2016 Diglin (http://www.diglin.com)
+ */
