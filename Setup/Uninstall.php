6bfd750d52e240ede8941b282480f38e54729c05
Sylvain Rayé <sly@diglin.com>
2016-08-29 13:22:03 +0200

    Feature #15: Add uninstall script

--- Setup/Uninstall.php
+++ Setup/Uninstall.php
@@ -7,3 +7,46 @@
  * @package     Diglin_
  * @copyright   Copyright (c) 2011-2016 Diglin (http://www.diglin.com)
  */
+
+use \Diglin\Username\Setup;
+use Magento\Customer\Model\Customer;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Eav\Setup\EavSetup;
+
+/**
+ * Class Uninstall
+ */
+class Uninstall implements Magento\Framework\Setup\UninstallInterface
+{
+    /**
+     * @var EavSetup
+     */
+    private $eavSetup;
+
+    /**
+     * InstallSchema constructor.
+     */
+    public function __construct(
+        EavSetup $eavSetup
+    ){
+        $this->eavSetup = $eavSetup;
+    }
+
+    /**
+     * @param SchemaSetupInterface $setup
+     * @param ModuleContextInterface $context
+     */
+    public function uninstall(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        $this->eavSetup->removeAttribute(Customer::ENTITY, 'username');
+
+        $setup->getConnection()->dropColumn($setup->getTable('customer_grid_flat'), 'username');
+        $setup->getConnection()->dropColumn($setup->getTable('quote'), 'customer_username');
+        $setup->getConnection()->dropColumn($setup->getTable('sales_order'), 'customer_username');
+
+        $setup->endSetup();
+    }
+}
